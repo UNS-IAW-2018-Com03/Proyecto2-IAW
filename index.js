a707b19e6399c1336f418c7bae9e84f576857d43
ManuelRios94 <you@example.com>
2018-04-24 09:29:47 -0300

    Proyecto Funcionando en el servidor

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', (req, res) => {
-  res.end('Holi!');
+//Obteniendo Rutas
+const routes = require('./routes');
+
+//Settings
+app.set('appName','R&R Solutions');
+app.set('views',__dirname + '/views');
+app.set('view engine','ejs');
+
+//Middlewares
+app.use(morgan('dev'));
+
+//Rutas
+app.use(routes);
+
+//Directorio Publico
+app.use(express.static(__dirname + '/public'));
+
+//Lo Dejo aca?
+app.get('*', (req, res) => {
+  res.end('Archivo No Encontrado.');
 })
 
 app.listen(3000, () => {
-  console.log('Servidor Funcionando.')
+  console.log('Servidor Funcionando.');
+  console.log('Nombre de la App: ',app.get('appName'));
 });
---
+++ public/js/modelReclamos.js
@@ -0,0 +1,17 @@
+var jsonReclamo = [
+				{
+					"titulo" : "Falta de Luz",
+					"imagen" : "src/img/icon1.png",
+					"id" : "-LM1" 
+				},
+				{
+					"titulo" : "Falta de Agua",
+					"imagen" : "src/img/icon2.png",
+					"id" : "-LM2" 
+				},
+				{
+					"titulo" : "Falta de Gas",
+					"imagen" : "src/img/icon3.png",
+					"id" : "-LM3" 
+				}
+];
